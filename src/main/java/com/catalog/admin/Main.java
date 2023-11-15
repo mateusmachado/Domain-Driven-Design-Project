6c77aa2f7c96c1a184823090c239c14181762936
Mateus <mmachado.csp@gmail.com>
2023-11-15 14:39:36 -0300

    Chore: format main class

--- src/main/java/com/catalog/admin/Main.java
+++ src/main/java/com/catalog/admin/Main.java
@@ -2,11 +2,8 @@ package com.catalog.admin;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.printf("Hello and welcome!");
-
         for (int i = 1; i <= 5; i++) {
-
             System.out.println("i = " + i);
         }
     }
