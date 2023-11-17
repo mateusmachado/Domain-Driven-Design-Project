98c0feae17859765c9f58961dca56a6b4a6a1aa2
Mateus <mmachado.csp@gmail.com>
2023-11-17 18:46:17 -0300

    chore: code improvements

--- domain/src/test/java/com/catalog/admin/domain/category/CategoryTest.java
+++ domain/src/test/java/com/catalog/admin/domain/category/CategoryTest.java
@@ -3,10 +3,10 @@ package com.catalog.admin.domain.category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CategoryTest {
+class CategoryTest {
 
     @Test
-    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
         final var expectedName = "Movies";
         final var expectedDescription = "Most watched category";
         final var expectedIsActive = true;
