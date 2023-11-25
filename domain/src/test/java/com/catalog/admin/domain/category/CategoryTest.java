6dc1e408bd39ffb76ba6c1e69327014ef92c48d0
Mateus <mmachado.csp@gmail.com>
2023-11-25 19:51:49 -0300

    feat: impl Unit Test

--- domain/src/test/java/com/catalog/admin/domain/category/CategoryTest.java
+++ domain/src/test/java/com/catalog/admin/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.catalog.admin.domain.category;
 
+import com.catalog.admin.domain.exceptions.DomainException;
+import com.catalog.admin.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +23,25 @@ class CategoryTest {
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    void givenAnIvalidNullName_whenCallNewCategory_thenInstantiateACategory() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () ->
+                actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
+
+
     }
 }
